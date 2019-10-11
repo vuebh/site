34bcfb81090822e34172f734aa6e4d8c6f4051fe
G3ra1d0 <geraldo-vitor@hotmail.com>
2019-10-11 20:42:22 -0300

    Include Sobre a comunidade Vue js BH

--- src/pages/Sobre.vue
+++ src/pages/Sobre.vue
@@ -1,12 +1,26 @@
 <template>
-  <q-page class="flex flex-top">
-    <h3>Sobre a comunidade VueJS de BH</h3>
+  <q-page class="q-pl-md font">
+    <h4>Sobre a comunidade VueJSBH:</h4>
+    <p>
+      VueJSBH é uma comunidade de usuários na qual utilizam o ecossistema Vue js no Estado
+      de Minas Gerais mais especificamente na cidade de Belo Horizonte. Somos uma comunidade
+      aberta e acolhemos tanto iniciantes quanto profissionais experientes que trabalham Vue js...
+    </p>
   </q-page>
 </template>
 
 <style>
+@import url("https://fonts.googleapis.com/css?family=Raleway&display=swap");
+
+h4 {
+  margin-bottom: 5px;
+}
+
+.font {
+  font-family: "Raleway", sans-serif;
+  font-size: 1.5em;
+}
 </style>
 
 <script>
-
 </script>
