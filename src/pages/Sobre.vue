adbd40d3719346d3c02b27767dda02927bd9b67c
G3ra1d0 <geraldo-vitor@hotmail.com>
2019-10-11 20:44:18 -0300

    Include sobre o que a comunidade faz

--- src/pages/Sobre.vue
+++ src/pages/Sobre.vue
@@ -6,6 +6,14 @@
       de Minas Gerais mais especificamente na cidade de Belo Horizonte. Somos uma comunidade
       aberta e acolhemos tanto iniciantes quanto profissionais experientes que trabalham Vue js...
     </p>
+    <h4>O que fazemos:</h4>
+    <p>
+      Realizamos <a target="_blank" href="https://www.meetup.com/pt-BR/Vuejs-at-BH/">encontros e eventos</a>
+      de Vue js na grande cidade de Belo Horizonte. Colaboramos com <a target="_blank" href="https://github.com/vuebh">projetos</a> open source,
+      disponibilizamos <a target="_blank" href="https://github.com/vuebh/vagas">vagas</a> na
+      cidade, ajudamos iniciantes tirando suas dúvidas no grupo do <a target="_blank" href="https://t.me/vuejsbh">telegram</a> e muito mais, visite nosso
+      <router-link tag="a" to="/blog">blog.</router-link>
+    </p>
   </q-page>
 </template>
 
